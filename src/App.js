c797b4962002fa1f6763f986f94235627d67d4fe
YujunSun0 <cons202@gmail.com>
2023-02-01 16:44:29 +0900

    첫 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [location, setLocation] = useState("");
+  const API_KEY = "968008af43c256a32e28fa8ce9d95a08";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+
+  const [result, setResult] = useState({})
+  
+  
+   const searchWeather = async (e) => {
+     if (e.key === "Enter") {
+       try {
+         const data = await axios({
+           method: "get",
+           url: url,
+         });
+         console.log(data);
+         setResult(data);
+         
+       } catch (err) {
+         console.log(err);
+       }
+     }
+   };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contents">
+        <input
+          placeholder="city name"
+          type="text"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyDown={searchWeather}
+        />
+        {(Object.keys(result).length === 0 )|| (
+          <div className="loadWeather">
+            <div className="city">{result.data.name}</div>
+            <img
+              src={`http://openweathermap.org/img/wn/${result.data.weather[0].icon}@2x.png`}
+              alt="icon"
+            />
+            <div className="temp">
+              {Math.round((result.data.main.temp - 273.15) * 10) / 10}°C
+            </div>
+            <div className="sky">{result.data.weather[0].main}</div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
